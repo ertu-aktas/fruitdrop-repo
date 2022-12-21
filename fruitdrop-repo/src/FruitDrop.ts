014af21fce22a904ab8a7659d288380ff0720377
ertu8820 <112567004+ertu8820@users.noreply.github.com>
2022-12-21 16:18:04 +0100

    added the player and movements, class attributes

--- fruitdrop-repo/src/FruitDrop.ts
+++ fruitdrop-repo/src/FruitDrop.ts
@@ -2,11 +2,23 @@ import { Game } from './GameLoop.js';
 
 import CanvasUtil from './CanvasUtil.js';
 import KeyListener from './KeyListener.js';
+import Player from './Player.js';
+import GameItem from './GameItem.js';
 
 export default class FruitDrop extends Game {
   private canvas: HTMLCanvasElement;
 
   private keyListener: KeyListener;
+
+  private player: Player;
+
+  private item: GameItem[] = [];
+
+  private score: number;
+
+  private timeLeft: number;
+
+  private timeToNextItem: number;
 
   public constructor(canvas: HTMLCanvasElement) {
     super();
@@ -14,13 +26,24 @@ export default class FruitDrop extends Game {
     this.canvas.height = window.innerHeight;
     this.canvas.width = window.innerWidth;
     this.keyListener = new KeyListener();
+
+    this.player = new Player(this.canvas.width, this.canvas.height);
+    this.score = 0;
+    this.timeLeft = 60000;
+    this.timeToNextItem = 500;
   }
 
   /**
    * Process all input. Called from the GameLoop.
    */
   public processInput(): void {
+    if (this.player.getPosX() > 0) {
+      if (this.keyListener.isKeyDown(KeyListener.KEY_LEFT)) this.player.move(0);
+    }
 
+    if (this.player.getPosX() < this.canvas.width - 100) {
+      if (this.keyListener.isKeyDown(KeyListener.KEY_RIGHT)) this.player.move(1);
+    }
   }
 
   /**
@@ -37,6 +60,7 @@ export default class FruitDrop extends Game {
    * Render all the elements in the screen.
    */
   public render(): void {
-
+    CanvasUtil.clearCanvas(this.canvas);
+    this.player.render(this.canvas);
   }
 }
